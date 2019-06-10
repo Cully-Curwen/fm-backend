e9702273957e06ce6603a90f940ead744a2d0e19
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-06-10 22:20:43 +0100

    query customerData working

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,18 @@
+const { ObjectId } = require('mongodb');
+const { getCustomerId, getMarketAdminId, getTraderAdminId } = require('../utils');
+const { APP_SECRET } = require('../secrets');
 
+async function customerData(parent, args, context, info) {
+  const Customers = context.db.collection('customers');
+  const _id = ObjectId(getCustomerId(context));
+
+  const customer = await Customers.findOne({ _id });
+  if (!customer) throw new Error('Token invalid');
+  
+  return customer;
+};
 
 module.exports = {
+  customerData,
 
 };
